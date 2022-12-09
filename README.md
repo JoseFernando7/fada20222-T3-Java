24fb00542f649e96fd6592b78eb2b4655992f5ca
Jose Fernando <jose.f72000@gmail.com>
2022-12-08 20:25:16 -0500

    Entrega taller 3

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ Se ha agregado una referencia al padre en las pruebas, puede hacer este cambio p
 @Setter
 private ArbolRojinegro father;
 ```
+
+## Sobre la entrega
+
+1- Las anotaciones de ```@Getter``` y ```@Setter``` no funcionaron correctamente (o no supimos utilizarlos) por lo que se usó los getters y setters por defecto de java.
+
+2- Las funciones de rotaciones (izquierda y derecha) funcionan correctamente cambiando la estructura de los árboles. Pero por alguna situación que no logramos arreglar la función de búsqueda bfs no la toma correctamente por lo que las funciones fallan los tests.
--- app/src/main/java/rojinegros/ArbolRojinegro.java
+++ app/src/main/java/rojinegros/ArbolRojinegro.java
@@ -1,33 +1,38 @@
 package rojinegros;
 
-import lombok.Getter;
-import lombok.Setter;
+/*import lombok.Getter;
+import lombok.Setter;*/
 
-import javax.naming.OperationNotSupportedException;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class ArbolRojinegro {
-    @Getter
-    @Setter
+    /*
+     * @Getter No me funcionó :'v (o no lo sé usar)
+     *
+     * @Setter private ArbolRojinegro izq;
+     *
+     * @Getter
+     *
+     * @Setter private ArbolRojinegro der;
+     *
+     * @Getter
+     *
+     * @Setter private int valor;
+     *
+     * @Getter
+     *
+     * @Setter private boolean black; //Si es negro True, en otro caso rojo
+     */
+
     private ArbolRojinegro izq;
-
-    @Getter
-    @Setter
     private ArbolRojinegro der;
-
-    @Getter
-    @Setter
+    private ArbolRojinegro father;
     private int valor;
+    private boolean black; // Si es negro True, en otro caso rojo
 
-    @Getter
-    @Setter
-    private boolean black; //Si es negro True, en otro caso rojo
-
-    public ArbolRojinegro(ArbolRojinegro izq,
-                          ArbolRojinegro der,
-                          int valor,
-                          boolean black) {
+    public ArbolRojinegro(ArbolRojinegro izq, ArbolRojinegro der,
+int valor, boolean black) { // <-- (Me estoy enfermando de ver esta línea así)
         this.izq = izq;
         this.der = der;
         this.valor = valor;
@@ -39,38 +44,150 @@ public class ArbolRojinegro {
         this.der = null;
         this.black = true;
     }
+
+    /**************** Getters y Setters ****************/
+    public ArbolRojinegro getIzq() {
+        return izq;
+    }
+
+    public void setIzq(ArbolRojinegro izq) {
+        this.izq = izq;
+    }
+
+    public ArbolRojinegro getDer() {
+        return der;
+    }
+
+    public void setDer(ArbolRojinegro der) {
+        this.der = der;
+    }
+
+    public ArbolRojinegro getFather() {
+        return father;
+    }
+
+    public void setFather(ArbolRojinegro father) {
+        this.father = father;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public boolean isBlack() {
+        return black;
+    }
+
+    public void setBlack(boolean black) {
+        this.black = black;
+    }
+
+    /**************** Getters y Setters ****************/
+
     /*
      * Metodos a implementar
      */
 
     public void insertar(int x) throws Exception {
-        throw new OperationNotSupportedException();
+        if (x >= this.valor) {
+            if (this.der == null) {
+                this.der = new ArbolRojinegro(null, null, x, false);
+                this.der.setFather(this);
+            } else {
+                this.der.insertar(x);
+            }
+        } else {
+            if (this.izq == null) {
+                this.izq = new ArbolRojinegro(null, null, x, false);
+                this.izq.setFather(this);
+            } else {
+                this.izq.insertar(x);
+            }
+        }
     }
 
     public int maximo() throws Exception {
-        throw new OperationNotSupportedException();
+        if (this.getDer() != null) {
+            return this.getDer().maximo();
+        } else {
+            return this.getValor();
+        }
     }
 
     public int minimo() throws Exception {
-        throw new OperationNotSupportedException();
+        if (this.getIzq() != null) {
+            return this.getIzq().minimo();
+        } else {
+            return this.getValor();
+        }
     }
 
     public ArbolRojinegro search(int x) throws Exception {
-        throw new OperationNotSupportedException();
+        if (this.valor == x) {
+            return this;
+        } else {
+            if (x >= this.valor) {
+                if (this.getDer() != null) {
+                    return this.getDer().search(x);
+                } else {
+                    return null;
+                }
+            } else {
+                if (this.getIzq() != null) {
+                    return this.getIzq().search(x);
+                } else {
+                    return null;
+                }
+            }
+        }
     }
 
     public void rotacionIzquierda(int x) throws Exception {
-        throw new OperationNotSupportedException();
+        ArbolRojinegro nodo = this.search(x);
+
+        if (nodo != null) {
+            ArbolRojinegro nodoDer = nodo.getDer();
+
+            nodo.setDer(nodoDer.getIzq());
+
+            if (nodoDer.getIzq() != null) {
+                nodoDer.getIzq().setFather(nodo);
+            }
+
+            nodoDer.setIzq(nodo);
+            nodo.setFather(nodoDer);
+        } else {
+            throw new Exception();
+        }
     }
 
-    public void rotacionDerecha(int x) throws  Exception {
-        throw new OperationNotSupportedException();
+    public void rotacionDerecha(int x) throws Exception {
+        ArbolRojinegro nodo = this.search(x);
+
+        if (nodo != null) {
+            ArbolRojinegro nodoIzq = nodo.getIzq();
+
+            nodo.setIzq(nodoIzq.getDer());
+
+            if (nodoIzq.getDer() != null) {
+                nodoIzq.getDer().setFather(nodo);
+            }
+
+            nodoIzq.setDer(nodo);
+            nodo.setFather(nodoIzq);
+        } else {
+            throw new Exception();
+        }
     }
 
     /*
-     *  Area de pruebas, no modificar.
+     * Area de pruebas, no modificar.
      */
-    //Verificaciones
+    // Verificaciones
     /*
      * Busqueda por amplitud para verificar arbol.
      */
@@ -94,8 +211,7 @@ public class ArbolRojinegro {
     }
 
     /*
-     * Recorrido inorder.
-     * Verifica propiedad de orden.
+     * Recorrido inorder. Verifica propiedad de orden.
      */
     public String inorden() {
         String recorrido = "";
